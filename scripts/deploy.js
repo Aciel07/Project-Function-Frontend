2a48d306af2c91e3599ad33e72bf15766af54801
Aciel <106684053+Aciel07@users.noreply.github.com>
2024-12-02 03:17:23 +0800

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,18 +1,12 @@
-const { ethers } = require("hardhat");
+const hre = require("hardhat");
 
 async function main() {
-  // Deploy the contract with an initial balance of 1 ETH (converted to wei)
-  const initBalance = ethers.utils.parseEther("1"); // 1 ETH in wei
+  const DonationBox = await hre.ethers.getContractFactory("DonationBox");
+  const donationBox = await DonationBox.deploy();
 
-  // Get the contract factory for Assessment
-  const Assessment = await ethers.getContractFactory("Assessment");
-  
-  // Deploy the contract with the initial balance
-  const assessment = await Assessment.deploy(initBalance);
-  await assessment.deployed();
+  await donationBox.deployed();
 
-  console.log(`Contract deployed at address: ${assessment.address}`);
-  console.log(`Initial balance of the contract: 1 ETH`);
+  console.log("DonationBox deployed to:", donationBox.address);
 }
 
 main().catch((error) => {
